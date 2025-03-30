0e0a93f4708326ec00917717011bbc32a2e7ba14
songhee <songhee9327@gmail.com>
2025-03-30 22:44:49 +0900

    Initial commit

---
+++ src/main/java/com/hufs/algoing/AlgoingApplication.java
@@ -0,0 +1,13 @@
+package com.hufs.algoing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgoingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hufs/algoing/AlgoingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hufs.algoing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
