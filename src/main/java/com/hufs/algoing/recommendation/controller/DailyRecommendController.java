b2894982d34217290e8795150b47afdc24e61bc1
dahyuuun <dahyun5243@gmail.com>
2025-05-13 17:14:26 +0900

    docs: dailyRecommend/weaknessRecommend api문서 추가 #21

--- src/main/java/com/hufs/algoing/recommendation/controller/DailyRecommendController.java
+++ src/main/java/com/hufs/algoing/recommendation/controller/DailyRecommendController.java
@@ -2,11 +2,15 @@ package com.hufs.algoing.recommendation.controller;
 
 import com.hufs.algoing.recommendation.dto.DailyRecommendDTO;
 import com.hufs.algoing.recommendation.service.DailyRecommendService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "데일리 문제 추천 API", description = "데일리 문제 추천 API")
 @RestController
 @RequestMapping("/dailyRecommendations")
 @RequiredArgsConstructor
@@ -15,6 +19,8 @@ public class DailyRecommendController {
     private final DailyRecommendService dailyRecommendService;
 
     // 일일 추천 문제 조회
+    @Operation(summary = "userId", description = "요청 받은 사용자의 티어와 선호 문제 유형을 기반으로 문제를 추천합니다.")
+    @Parameter(name = "long", description = "추천 받을 사용자 id")
     @GetMapping("/{userId}")
     public List<DailyRecommendDTO> getDailyRecommendations(@PathVariable Long userId) {
         // 서비스 호출하여 추천 문제 목록 가져오기
--- src/main/java/com/hufs/algoing/recommendation/controller/WeaknessRecommendController.java
+++ src/main/java/com/hufs/algoing/recommendation/controller/WeaknessRecommendController.java
@@ -6,9 +6,12 @@ import com.hufs.algoing.recommendation.dto.WeaknessRecommendDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 
+@Tag(name = "약점 기반 문제 추천 API", description = "약점 기반 문제 추천 API")
 @RestController
 @RequestMapping("/weaknessRecommendations")
 @RequiredArgsConstructor
@@ -17,6 +20,8 @@ public class WeaknessRecommendController {
 
     private final WeaknessRecommendService weaknessRecommendService;
 
+    @Operation(summary = "userId", description = "요청 받은 사용자의 약점을 기반으로 문제를 추천합니다.")
+    @Parameter(name = "long", description = "추천 받을 사용자 id")
     @GetMapping("/{userId}")
     public List<WeaknessRecommendDTO> getWeaknessRecommendations(@PathVariable Long userId){
         List<WeaknessRecommendDTO> recommendations = weaknessRecommendService.getWeaknessRecommendations(userId);
