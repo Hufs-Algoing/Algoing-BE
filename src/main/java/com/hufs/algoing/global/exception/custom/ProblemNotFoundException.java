e94235729d69071e77a893a9c6b2006336bfe7c6
dahyuuun <dahyun5243@gmail.com>
2025-05-21 21:02:30 +0900

    Settings: 에러 status 추가

--- src/main/java/com/hufs/algoing/global/exception/custom/ProblemNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.hufs.algoing.global.exception.custom;
-
-import com.hufs.algoing.global.code.BaseErrorCode;
-import com.hufs.algoing.global.exception.GeneralException;
-
-public class ProblemNotFoundException extends GeneralException {
-  public ProblemNotFoundException(BaseErrorCode errorCode) {super(errorCode);;
-  }
-}
